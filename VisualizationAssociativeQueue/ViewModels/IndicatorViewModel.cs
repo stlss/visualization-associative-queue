3ecbdf3de5d052dbf12294216c706a19a6d87a5e
IvakinAD <adivakin@edu.hse.ru>
2024-04-27 21:21:02 +0500

    feat: Добавлена вьюмодель индикаторов очереди.

---
+++ VisualizationAssociativeQueue/ViewModels/IndicatorViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Media;
+
+namespace VisualizationAssociativeQueue.ViewModels
+{
+    internal class IndicatorViewModel : ObservableObject
+    {
+        #region Название
+        private string _name = "NoName";
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+        #endregion
+
+        #region Значение
+        private object? _value = null; 
+        public object? Value
+        {
+            get => _value;
+            set
+            {
+                var isChangedProperty = SetProperty(ref _value, value);
+
+                if (isChangedProperty)
+                    OnPropertyChanged(nameof(DisplayValue));
+            }
+        }
+        #endregion
+
+        #region Отображаемое значение
+        public string DisplayValue { get => Value?.ToString() ?? "None"; }
+        #endregion
+
+        #region Цвет
+        private SolidColorBrush _solidColorBrush = Brushes.Black;
+        public SolidColorBrush SolidColorBrush
+        {
+            get => _solidColorBrush;
+            set => SetProperty(ref _solidColorBrush, value);
+        }
+        #endregion
+    }
+}
--- VisualizationAssociativeQueue/ViewModels/MainWindowViewModel.cs
+++ VisualizationAssociativeQueue/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,30 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Brushes = VisualizationAssociativeQueue.Models.Brushes;
 
 namespace VisualizationAssociativeQueue.ViewModels
 {
     internal class MainWindowViewModel : ObservableObject
     {
+        #region Свойства
 
+        #region Индикаторы очереди
+        public IndicatorViewModel IndicatorOperation { get; private set; }
+        public IndicatorViewModel IndicatorCount { get; private set; }
+        public IndicatorViewModel IndicatorFront { get; private set; }
+        public IndicatorViewModel IndicatorBack { get; private set; }
+        #endregion
+
+        #endregion
+
+        
+        public MainWindowViewModel()
+        {
+            #region Индикаторы очереди
+            IndicatorOperation = new() { SolidColorBrush = Brushes.Red };
+            IndicatorCount = new() { Name = "Count", Value = 0, SolidColorBrush = Brushes.Red };
+            IndicatorFront = new() { Name = "Front", SolidColorBrush = Brushes.Red };
+            IndicatorBack = new() { Name = "Back", SolidColorBrush = Brushes.Red };
+            #endregion
+        }
     }
 }
