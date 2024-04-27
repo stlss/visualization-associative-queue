57f2d40d01f077109e886e8e555bd42644c5799b
IvakinAD <adivakin@edu.hse.ru>
2024-04-27 21:43:52 +0500

    feat: Добавлены команды к кнопкам.

--- VisualizationAssociativeQueue/ViewModels/MainWindowViewModel.cs
+++ VisualizationAssociativeQueue/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Brushes = VisualizationAssociativeQueue.Models.Brushes;
 
 namespace VisualizationAssociativeQueue.ViewModels
@@ -7,7 +8,7 @@ namespace VisualizationAssociativeQueue.ViewModels
     {
         #region Свойства
 
-        #region Индикаторы очереди
+        #region Индикаторы
         public IndicatorViewModel IndicatorOperation { get; private set; }
         public IndicatorViewModel IndicatorCount { get; private set; }
         public IndicatorViewModel IndicatorFront { get; private set; }
@@ -16,14 +17,70 @@ namespace VisualizationAssociativeQueue.ViewModels
 
         #endregion
 
-        
+        #region Команды
+
+        #region Добавить элемент
+        public RelayCommand<string?> PushCommand { get; private set; }
+
+        private void ExecutePushCommand(string? strNumber)
+        {
+
+        }
+
+        private bool CanExecutePushCommand(string? strNumber) => int.TryParse(strNumber, out _);
+        #endregion
+
+        #region Удалить элемент
+        public RelayCommand PopCommand { get; private set; }
+
+        private void ExecutePopCommand()
+        {
+
+        }
+
+        private bool CanExecutePopCommand() => false;
+        #endregion
+
+        #region
+        #endregion
+
+        #region Очистить очередь
+        public RelayCommand ClearCommand { get; private set; }
+
+        private void ExecuteClearCommand()
+        {
+
+        }
+        #endregion
+
+        #region Сгенерировать очередь
+        public RelayCommand<string?> GenerateCommand { get; private set; }
+
+        private void ExecuteGenerateCommand(string? strNumber)
+        {
+
+        }
+
+        private bool CanExecuteGenerateCommand(string? strNumber) => int.TryParse(strNumber, out _);
+        #endregion
+
+        #endregion
+
+
         public MainWindowViewModel()
         {
-            #region Индикаторы очереди
+            #region Индикаторы
             IndicatorOperation = new() { SolidColorBrush = Brushes.Red };
             IndicatorCount = new() { Name = "Count", Value = 0, SolidColorBrush = Brushes.Red };
             IndicatorFront = new() { Name = "Front", SolidColorBrush = Brushes.Red };
             IndicatorBack = new() { Name = "Back", SolidColorBrush = Brushes.Red };
+            #endregion
+
+            #region Команды
+            PushCommand = new(ExecutePushCommand, CanExecutePushCommand);
+            PopCommand = new(ExecutePopCommand, CanExecutePopCommand);
+            ClearCommand = new(ExecuteClearCommand);
+            GenerateCommand = new(ExecuteGenerateCommand, CanExecuteGenerateCommand);
             #endregion
         }
     }
