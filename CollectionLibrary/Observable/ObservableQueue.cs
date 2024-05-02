7fb4cfb763ff0667d1108ab05cec972f448a8ef2
IvakinAD <a.ivakin2014@gmail.com>
2024-05-02 12:47:47 +0500

    feat: Добавлены наблюдаемые стек и очередь.

---
+++ CollectionLibrary/Observable/ObservableQueue.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Specialized;
+
+namespace CollectionLibrary.Observable
+{
+    public class ObservableQueue<T> : Queue<T>, INotifyCollectionChanged
+    {
+        #region Реализация интерфейса INotifyCollectionChanged
+        public event NotifyCollectionChangedEventHandler? CollectionChanged;
+
+        protected void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            CollectionChanged?.Invoke(this, e);
+        }
+        #endregion
+
+        #region Сокрытие Enqueue, Dequeue, TryDequeue и Clear
+        public new void Enqueue(T item)
+        {
+            base.Enqueue(item);
+            OnCollectionChanged(new(NotifyCollectionChangedAction.Add, item));
+        }
+
+        public new T Dequeue()
+        {
+            if (Count == 0)
+                ExceptionWizard.ThrowEmptyCollection(this);
+
+            var item = base.Dequeue();
+            OnCollectionChanged(new(NotifyCollectionChangedAction.Remove, item));
+
+            return item;
+        }
+
+        public new bool TryDequeue(out T? result)
+        {
+            if (Count == 0)
+            {
+                result = default;
+                return false;
+            }
+
+            result = Dequeue();
+            return true;
+        }
+
+        public new void Clear()
+        {
+            base.Clear();
+            OnCollectionChanged(new(NotifyCollectionChangedAction.Reset));
+        }
+        #endregion
+    }
+}
---
+++ CollectionLibrary/Observable/ObservableStack.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Specialized;
+
+namespace CollectionLibrary.Observable
+{
+    public class ObservableStack<T> : Stack<T>, INotifyCollectionChanged
+    {
+        #region Реализация интерфейса INotifyCollectionChanged
+        public event NotifyCollectionChangedEventHandler? CollectionChanged;
+
+        protected void OnCollectionChanged(NotifyCollectionChangedEventArgs e) 
+        { 
+            CollectionChanged?.Invoke(this, e);
+        }
+        #endregion
+
+        #region Сокрытие Push, Pop, TryPop и Clear
+        public new void Push(T item)
+        {
+            base.Push(item);
+            OnCollectionChanged(new(NotifyCollectionChangedAction.Add, item));
+        }
+
+        public new T Pop() 
+        {
+            if (Count == 0)
+                ExceptionWizard.ThrowEmptyCollection(this);
+
+            var item = base.Pop();
+            OnCollectionChanged(new(NotifyCollectionChangedAction.Remove, item));
+
+            return item;
+        }
+
+        public new bool TryPop(out T? result)
+        {
+            if (Count == 0)
+            {
+                result = default;
+                return false;
+            }
+
+            result = Pop();
+            return true;
+        }
+
+        public new void Clear()
+        {
+            base.Clear();
+            OnCollectionChanged(new(NotifyCollectionChangedAction.Reset));
+        }
+        #endregion
+    }
+}
